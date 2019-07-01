d33b5467e9c060351a2686791c2541da26176f64
Artanidos <artanidos@gmail.com>
2019-07-01 19:21:18 +0200

    switched to epub 3

--- generator.py
+++ generator.py
@@ -35,18 +35,18 @@ def createEpub(output, book, win):
     dir = mkdtemp()
     guid = str(uuid.uuid4())
     copyAssets(dir, book.theme)
-    os.mkdir(os.path.join(dir, "OEBPS", "parts"))
-    os.mkdir(os.path.join(dir, "OEBPS", "images"))
+    os.mkdir(os.path.join(dir, "EPUB", "parts"))
+    os.mkdir(os.path.join(dir, "EPUB", "images"))
     os.mkdir(os.path.join(dir, "META-INF"))
     path = os.getcwd()
 
     copyImages(dir, book)
     writeMimetype(dir)
     writeContainer(dir)
+
     generatePackage(dir, book, guid)
-    generateParts(dir, book)
-    generateToc(dir, book)
-    generateTocNcx(dir, book, guid)
+    toc = generateParts(dir, book)
+    generateToc(dir, book, toc)
 
     os.chdir(dir)
     files = getAllFiles(dir)
@@ -78,12 +78,12 @@ def writeMimetype(dir):
 def writeContainer(dir):
     midir = os.path.join(dir, "META-INF")
     with open(os.path.join(midir, "container.xml"), "w") as f:
-        f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-        f.write("<container xmlns=\"urn:oasis:names:tc:opendocument:xmlns:container\" version=\"1.0\">\n")
-        f.write("    <rootfiles>\n")
-        f.write("        <rootfile full-path=\"OEBPS/package.opf\" media-type=\"application/oebps-package+xml\"/>\n")
-        f.write("    </rootfiles>\n")
-        f.write("</container>\n")
+        f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+        f.write("<container xmlns=\"urn:oasis:names:tc:opendocument:xmlns:container\" version=\"1.0\">")
+        f.write("  <rootfiles>")
+        f.write("    <rootfile full-path=\"EPUB/package.opf\" media-type=\"application/oebps-package+xml\"/>")
+        f.write("  </rootfiles>")
+        f.write("</container>")
 
 
 def generatePackage(dir, book, uuid):
@@ -91,17 +91,12 @@ def generatePackage(dir, book, uuid):
     context["uuid"] = uuid
     context["lang"] = book.language
     context["title"] = book.name
-    context["date"] = datetime.datetime.now().strftime("%Y-%m-%d")
+    context["date"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
     context["version"] = QCoreApplication.applicationVersion()
     context["creator"] = book.creator
     context["publisher"] = book.publisher
     items = []
-    spine = ["toc"]
-    item = {}
-    item["href"] = os.path.join("parts", "toc.xhtml")
-    item["id"] = "toc"
-    item["type"] = "application/xhtml+xml"
-    items.append(item)
+    spine = []
 
     for part in book._parts:
         item = {}
@@ -111,7 +106,7 @@ def generatePackage(dir, book, uuid):
         items.append(item)
         spine.append(part.name)
 
-    for root, dirs, files in os.walk(os.path.join(dir, "OEBPS", "images")):
+    for root, dirs, files in os.walk(os.path.join(dir, "EPUB", "images")):
         for file in files:
             filename, extension = os.path.splitext(file)
             item = {}
@@ -120,7 +115,6 @@ def generatePackage(dir, book, uuid):
             item["type"] = "image/" + extension[1:]
             items.append(item)
 
-    context["spine"] = ["nav", "toc"]
     context["items"] = items
     context["spine"] = spine
     path = os.getcwd()
@@ -129,24 +123,31 @@ def generatePackage(dir, book, uuid):
     ]
     eng = Engine(dirs = dirs, debug = False)
     xml = eng.render_to_string("package.opf", context = context)
-    with open(os.path.join(dir, "OEBPS", "package.opf"), "w") as f:
+    with open(os.path.join(dir, "EPUB", "package.opf"), "w") as f:
         f.write(xml)
 
 
-def setAnchors(lines):
-    text = ""
-    for line in lines:
-        if not line:
-            continue
-        if line[0] == "#":
-            link = line.lstrip("#").replace(" ", "-").lower()
-            text += line + '<a name="' + link + '"/>  \n'
-        else:
-            text += line + "\n"
-    return text
+# def setAnchors(lines):
+#     text = ""
+#     for line in lines:
+#         if not line:
+#             continue
+#         if line[0] == "#":
+#             link = line.lstrip("#").strip().replace(" ", "-").lower()
+#             text += '<a href="#' + link + '"></a>\n' + line + "\n"
+#         else:
+#             text += line + "\n"
+#     return text
 
 
 def generateParts(dir, book):
+    toc = []
+    item = {}
+    item["href"] = "toc.xhtml"
+    item["name"] = "Toc"
+    item["id"] = "nav"
+    item["parts"] = []
+    toc.append(item)
     path = os.getcwd()
     dirs = [
         path + "/themes/" + book.theme + "/layout",
@@ -156,95 +157,70 @@ def generateParts(dir, book):
         context = {}
         with open(os.path.join(book.source_path, "parts", part.src), "r") as i:
             text = i.read()
-        text = setAnchors(text.split("\n"))
-        context["content"] = mark_safe(markdown(text, html4tags = False, extras=["fenced-code-blocks", "tables"]))
+        list = getCaptions(text, part.name)
+        for item in list:
+            toc.append(item)
+        context["content"] = mark_safe(markdown(text, html4tags = False, extras=["fenced-code-blocks", "tables", "header-ids"]))
         xhtml = eng.render_to_string("template.xhtml", context = context)
-        with open(os.path.join(dir, "OEBPS", "parts", part.name + ".xhtml"), "w") as f:
+        with open(os.path.join(dir, "EPUB", "parts", part.name + ".xhtml"), "w") as f:
                 f.write(xhtml)
+    return toc
 
 
 def copyAssets(dir, theme):
     path = os.getcwd()
-    shutil.copytree(os.path.join(path, "themes", theme, "assets"), os.path.join(dir, "OEBPS"))
+    shutil.copytree(os.path.join(path, "themes", theme, "assets"), os.path.join(dir, "EPUB"))
 
 
 def copyImages(dir, book):
     for root, dirs, files in os.walk(os.path.join(book.source_path, "images")):
         for file in files:
-            shutil.copy(os.path.join(book.source_path, "images", file), os.path.join(dir, "OEBPS", "images"))
+            shutil.copy(os.path.join(book.source_path, "images", file), os.path.join(dir, "EPUB", "images"))
 
 
-def getCaptions(lines):
+def countHash(text):
+    count = 0
+    for letter in text:
+        if letter == "#":
+            count += 1
+        else:
+            break
+    return count
+
+
+def getCaptions(text, part_name):
+    nodes = []
     list = []
-    for line in lines:
+    for line in text.split("\n"):
         if not line:
             continue
-        if line[0] == "#":
-            list.append(line.lstrip("#"))
+        c = countHash(line)
+        if c > 0:
+            line = line.strip("#").strip()
+            item = {}
+            item["href"] = part_name + ".xhtml#" + line.lower().replace(" ", "-")
+            item["name"] = line
+            item["id"] = part_name
+            item["parts"] = []
+            if len(nodes) < c:
+                nodes.append(item)
+            else:
+                nodes[c - 1] = item
+            if c == 1:
+                list.append(item)
+            else:
+                nodes[c - 2]["parts"].append(item)
     return list
 
 
-def generateToc(dir, book):
+def generateToc(dir, book, parts):
     path = os.getcwd()
-    parts = []
-    toc = {}
-    toc["href"] = "toc.xhtml"
-    toc["name"] = "Toc"
-    parts.append(toc)
     context = {}
-    for part in book._parts:
-        item = {}
-        item["href"] = part.name + ".xhtml"
-        item["name"] = part.name
-        parts.append(item)
-
-        with open(os.path.join(book.source_path, "parts", part.src), "r") as i:
-            text = i.read()
-        list = getCaptions(text.split("\n"))
-        for l in list:
-            item = {}
-            item["href"] = part.name + ".xhtml#" + l.lower().replace(" ", "-")
-            item["name"] = l
-            parts.append(item)
-
     context["parts"] = parts
     dirs = [
         path + "/themes/" + book.theme + "/layout",
     ]
     eng = Engine(dirs = dirs, debug = False)
     xhtml = eng.render_to_string("toc.xhtml", context = context)
-    with open(os.path.join(dir, "OEBPS", "parts", "toc.xhtml"), "w") as f:
-        f.write(xhtml)
-
-
-def generateTocNcx(dir, book, uuid):
-    order = 0
-    path = os.getcwd()
-    parts = []
-    toc = {}
-    toc["href"] = os.path.join("parts", "toc.xhtml")
-    toc["id"] = "navPoint-" + str(order)
-    toc["label"] = "Toc"
-    toc["order"] = str(order)
-    order = order + 1
-    parts.append(toc)
-    context = {}
-    context["uuid"] = uuid
-    context["title"] = book.name
-    for part in book._parts:
-        item = {}
-        item["href"] = os.path.join("parts", part.name + ".xhtml")
-        item["id"] = "navPoint-" + str(order)
-        item["label"] = part.name
-        item["order"] = str(order)
-        parts.append(item)
-        order = order + 1
-
-    context["navs"] = parts
-    dirs = [
-        path + "/themes/" + book.theme + "/layout",
-    ]
-    eng = Engine(dirs = dirs, debug = False)
-    xhtml = eng.render_to_string("toc.ncx", context = context)
-    with open(os.path.join(dir, "OEBPS", "toc.ncx"), "w") as f:
+    with open(os.path.join(dir, "EPUB", "parts", "toc.xhtml"), "w") as f:
         f.write(xhtml)
