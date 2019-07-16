db6221dc70c9284388e7bcc9be6756cbaf2717fd
Artanidos <artanidos@gmail.com>
2019-07-16 14:47:39 +0200

    fixes

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ The books are targetted towards Amazon Kindle.
 Because of the fact that the program is written in Python using PyQt5 it should run on all mayor platforms. With some changed (menu) it should also run on tablets.
 
 If you have got a feature request don't hesitate to contact me at <artanidos@gmail.com>
+
+## Donations
+If you like to support my work on the AnimationMaker you are invited to make a donation.  
+
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://paypal.me/artanidos)
+
--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ cp -r dist/main/* packages/com.vendor.product/data/bin
 cp -r themes/* packages/com.vendor.product/data/themes
 cp AppRun packages/com.vendor.product/data
 mv packages/com.vendor.product/data/bin/main packages/com.vendor.product/data/bin/EbookCreator
-/home/art/Qt/Tools/QtInstallerFramework/3.1/bin/binarycreator -f -c config/config.xml -p packages EbookCreator-Linux-1.0.0.Setup
+/home/art/Qt/Tools/QtInstallerFramework/3.1/bin/binarycreator -f -c config/config.xml -p packages EbookCreator-Linux-1.0.2.Setup
 
